673c26f32f289ef8560db577a46d56c68516421e
mohitrakhade11 <mrakhade7@gmail.com>
2023-04-15 12:06:35 +0530

    txt

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,66 @@
+import User from "../models/user.model.js";
+import createError from "../utils/createError.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+export const register = async (req, res, next) => {
+  try {
+    const hash = bcrypt.hashSync(req.body.password, 5);
+    const newUser = new User({
+      // ...req.body,
+      username: req.body.username,
+  email: req.body.email,
+  password: req.body.password,
+  country: req.body.country,
+      password: hash
+    });
+    
+    let response = await newUser.save();
+    console.log(req.body)
+    console.log(response);
+    res.status(201).send("User has been created.");
+  } catch (err) {
+    console.log(err)
+    next(err);
+
+  }
+};
+export const login = async (req, res, next) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+
+    if (!user) return next(createError(404, "User not found!"));
+
+    const isCorrect = bcrypt.compareSync(req.body.password, user.password);
+    if (!isCorrect)
+      return next(createError(400, "Wrong password or username!"));
+
+    const token = jwt.sign(
+      {
+        id: user._id,
+        isSeller: user.isSeller,
+      },
+      process.env.JWT_KEY
+    );
+
+    const { password, ...info } = user._doc;
+    res
+      .cookie("accessToken", token, {
+        httpOnly: true,
+      })
+      .status(200)
+      .send(info);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const logout = async (req, res) => {
+  res
+    .clearCookie("accessToken", {
+      sameSite: "none",
+      secure: true,
+    })
+    .status(200)
+    .send("User has been logged out.");
+};
---
+++ client/src/components/footer/Footer.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import "./Footer.scss";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <div className="container">
+        <div className="top">
+          <div className="item">
+            <h2>Categories</h2>
+            <span>Graphics & Design</span>
+            <span>Digital Marketing</span>
+            <span>Writing & Translation</span>
+            <span>Video & Animation</span>
+          </div>
+          <div className="item">
+            <h2>About</h2>
+            <span>Press & News</span>
+            <span>Partnerships</span>
+            <span>Privacy Policy</span>
+          </div>
+          <div className="item">
+            <h2>Support</h2>
+            <span>Help & Support</span>
+            <span>Trust & Safety</span>
+          </div>
+          <div className="item">
+            <h2>Community</h2>
+            <span>Customer Success Stories</span>
+            <span>Community hub</span>
+            <span>Forum</span>
+          </div>
+          <div className="item">
+            <h2>More From SkillWorks</h2>
+            <span>SkillWorks Business</span>
+            <span>SkillWorks Pro</span>
+          </div>
+        </div>
+        <hr />
+        <div className="bottom">
+          <div className="left">
+            <h2>SkillWorks</h2>
+            <span>© SkillWorks International Ltd. 2023</span>
+          </div>
+          <div className="right">
+            <div className="social">
+              <img src="/img/twitter.png" alt="" />
+             
+              <img src="/img/linkedin.png" alt="" />
+
+              <img src="/img/instagram.png" alt="" />
+            </div>
+            <div className="link">
+              <img src="/img/language.png" alt="" />
+              <span>English</span>
+            </div>
+            <div className="link">
+              <img src="/img/coin.png" alt="" />
+              <span>USD</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
